23d79a6eaa334a03b5bc3d1dd862986169c97b39
Hector Ledesma <41846079+mazi89@users.noreply.github.com>
2020-04-02 15:56:33 -0400

    Create Rect model in rect.h

---
+++ AlgorithmVisualizer/Model/rect.h
@@ -0,0 +1,33 @@
+#pragma once
+
+/*
+	Based on what little I've seen, right about every single thing that is rendered into the screen using SDL, is done so with the power of rectangles.
+	SDL_Rects only ask for:
+		1.: Dimensions	->	w, h.
+		2.: Position	->	x, y.
+		3.:	Color		->	r, g, b, alpha.
+
+	Using a user-define type allows us to set all these properties to private once defined, as well as keep any functions contained to each individual object.
+
+	It would also let us create texture in a much more organized manner.
+*/
+
+#include "window.h"
+#include <string>
+
+class Rect {
+public:
+	Rect(int w, int h, int x, int y, int r, int g, int b, int a);
+	~Rect();
+
+	// Must always have our function to render into the screen.
+	void draw() const; // If a function will not alter any of the object's properties (or at least shouldn't) make sure to mark it as const.
+
+	// Function to listen for input.
+	void pollEvents(SDL_Event& event);
+
+private:
+	int _w, _h;
+	int _x, _y;
+	int _r, _g, _b, _a;
+};
