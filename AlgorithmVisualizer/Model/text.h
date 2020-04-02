369aabdb588bd288e8d507d22f4b04d2341dbde9
Hector Ledesma <41846079+mazi89@users.noreply.github.com>
2020-04-02 03:36:22 -0400

    Create text.h file.

---
+++ AlgorithmVisualizer/Model/text.h
@@ -0,0 +1,5 @@
+#pragma once
+/*
+	With SDL, we can usue the SDL TTF library extension in order to handle rendering text (using fonts) onto the screen.
+	TTF_OpenFont requires the font path, and the font size.
+*/
