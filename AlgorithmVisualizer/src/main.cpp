f6520f8cf5b6be538697382c24ab63407e308dec
Hector Ledesma <41846079+mazi89@users.noreply.github.com>
2020-04-01 23:26:46 -0400

    Include all SDL Library dependency files. Succesfully initialize SDL.

--- AlgorithmVisualizer/src/main.cpp
+++ AlgorithmVisualizer/src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <SDL.h>
+#undef main
 
-int main() {
+int main(int argc, char *argv) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cerr << "SDL failed to initialize\n";
+	}
+	else {
+		std::cout << "Success!\n";
+	}
 
+	return 0;
 }
